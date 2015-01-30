8acc56b958ccc8558d300f71cfce292ce34a5bec
fabienduhamel <fabienduhamel@users.noreply.github.com>
2015-01-30 19:34:28 +0100

    added main packages and omzsh install

--- linux/setup.sh
+++ linux/setup.sh
@@ -2,5 +2,12 @@
 
 # this scripts runs the very essential commands for my new linux installation(s)
 
+# main packages
+sudo apt-get install terminator zsh
+
+# oh my zsh
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+
+# default shell
 chsh -s /bin/zsh
 
