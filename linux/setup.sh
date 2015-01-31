810a0f0d7c90725f091a3dee8281e47509bea0a4
fabienduhamel <fabienduhamel@users.noreply.github.com>
2015-01-31 14:44:54 +0100

    added vim in setup

--- linux/setup.sh
+++ linux/setup.sh
@@ -3,11 +3,10 @@
 # this scripts runs the very essential commands for my new linux installation(s)
 
 # main packages
-sudo apt-get install terminator zsh
+sudo apt-get install terminator zsh vim
 
 # oh my zsh
 wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 
 # default shell
 chsh -s /bin/zsh
-
