45490ddb87049b71e68094b4e20e83acc234f483
fabienduhamel <fabienduhamel@users.noreply.github.com>
2015-01-31 14:10:53 +0100

    added directory with blanks support

--- config-files.sh
+++ config-files.sh
@@ -7,204 +7,235 @@ REMOVE=0
 INSTALL=0
 BACKUP_DIR="./backup"
 INSTALL_LIST_FILE="to_install"
+TMP_INSTALL_LIST_FILE="to_install_tmp"
 
 function show_help {
-	echo "-c : create an environment"
-	echo "-e : select an environment"
-	echo "-a : track a file"
-	echo "-r : untrack a file"
-	echo "-i : install an environment"
-	echo "Only -a or -i or -r argument"
-	echo ""
-	echo "Usage:"
-	echo "1. Create an environment directory here with -c <environment>"
-	echo "2. Add your file or directory in the environment using -a option to track it (relative path in /home/$USER)"
-	echo "3. Use -i to symlink it in your system (it backups your current files automatically in your environment directory)"
-	echo
-	echo "Example:"
-	echo "./config-files.sh -c linux"
-	echo "./config-files.sh -e linux -a .bashrc"
-	echo "./config-files.sh -e linux -i"
+    echo "-c : create an environment"
+    echo "-e : select an environment"
+    echo "-a : track a file"
+    echo "-r : untrack a file"
+    echo "-i : install an environment"
+    echo "Only -a or -i or -r argument"
+    echo ""
+    echo "Usage:"
+    echo "1. Create an environment directory here with -c <environment>"
+    echo "2. Add your file or directory in the environment using -a option to track it (in your $HOME)"
+    echo "3. Use -i to symlink all your files in your system (it backups your current files automatically in your environment directory)"
+    echo
+    echo "Example:"
+    echo "./config-files.sh -c linux"
+    echo "./config-files.sh -e linux -a \"~/.loli/Installed\ Packages/\""
+    echo "./config-files.sh -e linux -a ~/.bashrc"
+    echo "./config-files.sh -e linux -i"
 }
 
-function create {
-	mkdir $ENVIRONMENT
-	touch $ENVIRONMENT/$INSTALL_LIST_FILE
+function path_without_home_path {
+    home_path="/home/$USER/"
+    substring=${1#$home_path}
+    home_path="~/"
+    substring=${substring#$home_path}
+    echo $substring
 }
 
-function check_existence_of_element_in_list {
-	element_to_add=$1
-	cat $INSTALL_LIST_FILE | while read -r element; do
-		if [ $element = $element_to_add ]; then
-			echo 1
-			return
-		fi
-	done
-	echo 0
+function create_environment {
+    mkdir $ENVIRONMENT
+    touch $ENVIRONMENT/$INSTALL_LIST_FILE
 }
 
 function add {
-	element_to_add=$1
-	existing_element=$(check_existence_of_element_in_list $element_to_add)
-	if [[ "$existing_element" = 1 ]]; then
-		echo "$element_to_add already added. Exiting"
-		return
-	else
-		echo "$element_to_add doesn't exist. Adding"
-		if [ -e ./$element_to_add ]; then
-			echo "$element_to_add already exists for this environment ($ENVIRONMENT). Exiting"
-			exit 1
-		fi
-		echo "cp -r $HOME/$element_to_add ."
-		cp $HOME/$element_to_add .
-		echo -e "$element_to_add" >> $INSTALL_LIST_FILE
-	fi
+    element_to_add=$(path_without_home_path "$1")
+    if grep -Fxq "$element_to_add" $INSTALL_LIST_FILE; then
+        echo "$element_to_add already added. Exiting"
+
+        return
+    fi
+    echo "$element_to_add doesn't exist. Adding"
+    if [ -e "./$element_to_add" ]; then
+        echo "$element_to_add already exists for environment $ENVIRONMENT. Exiting"
+
+        exit 1
+    fi
+    echo "Retrieving $element_to_add"
+    retrieve "$element_to_add"
+    echo -e "$element_to_add" >> $INSTALL_LIST_FILE
+}
+
+function retrieve {
+    element_to_retrieve="$1"
+    environment_pwd=`pwd`
+    cd
+    pwd
+    cmd="rsync -aR $element_to_retrieve \"$environment_pwd\""
+    echo $cmd
+    eval $cmd
+    cd $environment_pwd
 }
 
 function remove {
-	element_to_remove=$1
-	existing_element=$(check_existence_of_element_in_list $element_to_remove)
-	if [[ "$existing_element" = 1 ]]; then
-		echo "$element_to_remove exists. Removing"
-		sed -i /^$element_to_remove$/d $INSTALL_LIST_FILE
-	else
-		echo "$element_to_remove doesn't already exist. Exiting"
-		return
-	fi
+    element_to_remove=$(path_without_home_path "$1")
+    if grep -Fxq "$element_to_remove" $INSTALL_LIST_FILE; then
+        echo "$element_to_remove exists. Removing"
+        echo "grep -Fxv \"$element_to_remove\" $INSTALL_LIST_FILE > $TMP_INSTALL_LIST_FILE"
+        grep -Fxv "$element_to_remove" $INSTALL_LIST_FILE > $TMP_INSTALL_LIST_FILE
+        echo "mv $TMP_INSTALL_LIST_FILE $INSTALL_LIST_FILE"
+        mv $TMP_INSTALL_LIST_FILE $INSTALL_LIST_FILE
+    else
+        echo "$element_to_remove doesn't already exist. Exiting"
+
+        return
+    fi
 }
 
 function install_environment {
-	echo "Install $ENVIRONMENT"
+    echo "Install $ENVIRONMENT"
 
-	cat $INSTALL_LIST_FILE | while read -r element; do
-		install "$element"
-	done
+    cat $INSTALL_LIST_FILE | while read -r element; do
+        install "$element"
+    done
 }
 
 function create_backup_dir {
-	if [ -d $BACKUP_DIR ]; then
-		return
-	else
-		mkdir $BACKUP_DIR
-	fi
-	if [ ! -d $BACKUP_DIR ]; then
-		echo "$BACKUP_DIR cannot be created. Exiting."
-		exit 1
-	fi
+    if [ -d $BACKUP_DIR ]; then
+
+        return
+    else
+        mkdir $BACKUP_DIR
+    fi
+    if [ ! -d $BACKUP_DIR ]; then
+        echo "$BACKUP_DIR cannot be created. Exiting."
+
+        exit 1
+    fi
 }
 
 function backup {
-	element=$1
-	echo "cp -r --parents \"$HOME/$element\" $BACKUP_DIR"
-	cp -r --parents "$HOME/$element" $BACKUP_DIR
-	if [ ! -e "$BACKUP_DIR/$HOME/$element" ]; then
-		echo "$element cannot be backuped. Exiting."
-		exit 1
-	fi
+    element_to_backup="$1"
+    environment_pwd=`pwd`
+    cd
+    pwd
+    cmd="rsync -aR $element_to_backup \"$environment_pwd/$BACKUP_DIR\""
+    echo $cmd
+    eval $cmd
+    cd $environment_pwd
 }
 
 function install {
-	element=$1
-	if [ -L "$HOME/$element" ]; then
-		echo "$HOME/$element is already a link. Skipping"
-		return
-	fi
-	if [ -e "$HOME/$element" ]; then
-		create_backup_dir
-		echo "$HOME/$element exists. Backuping to $BACKUP_DIR/$element"
-		backup "$element"
-		echo "rm -r $HOME/$element"
-		rm -r "$HOME/$element"
-	else
-		echo "$element doesn't exist in system. No backup"
-	fi
-	link "$element"
+    element=$1
+    existing_link_cmd="test -L $HOME/$element"
+    if eval $existing_link_cmd; then
+        echo "$HOME/$element is already a link. Skipping"
+
+        return
+    fi
+    existing_element_cmd="test -e $HOME/$element"
+    if eval $existing_element_cmd; then
+        create_backup_dir
+        echo "$HOME/$element exists. Backuping to $BACKUP_DIR/$element"
+        backup "$element"
+        rm_cmd="rm -r $HOME/$element"
+        echo $rm_cmd
+        eval $rm_cmd
+    else
+        echo "$element doesn't exist in system. No backup"
+    fi
+    link "$element"
 }
 
 function link {
-	element=$1
-	target="$PWD/$element"
-	link_name="$element"
-	echo "Creating link for $element"
-	echo "ln -s \"$target\" \"$HOME/$link_name\""
-	ln -s "$target" "$HOME/$link_name"
+    element="$1"
+    target="$PWD/$element"
+    link_name="$element"
+    link_name_without_trailing_slash=${link_name%/}
+    link_cmd="ln -s $target $HOME/$link_name_without_trailing_slash"
+    echo "Creating link for $element"
+    echo $link_cmd
+    eval $link_cmd
 }
 
 while getopts ":c:?e:?a:?i?r:?" opt; do
-	case $opt in
-		e)
-			[ $CREATE = 1 ] &&
-			{
-				show_help
-				exit 1
-			}
-			UPDATE=1
-			ENVIRONMENT=$OPTARG
-			;;
-		c)
-			[ $UPDATE = 1 ] || [ $ADD = 1 ] || [ $REMOVE = 1 ] || [ $INSTALL = 1 ] &&
-			{
-				show_help
-				exit 1
-			}
-			CREATE=1
-			ENVIRONMENT=$OPTARG
-			;;
-		a)
-			[ $REMOVE = 1 ] || [ $INSTALL = 1 ] || [ $CREATE = 1 ] || [ $UPDATE = 0 ] &&
-			{
-				show_help
-				exit 1
-			}
-			ADD=1
-			ARG=$OPTARG
-			;;
-		r)
-			[ $ADD = 1 ] || [ $INSTALL = 1 ] || [ $CREATE = 1 ] || [ $UPDATE = 0 ] &&
-			{
-				show_help
-				exit 1
-			}
-			REMOVE=1
-			ARG=$OPTARG
-			;;
-		i)
-			[ $REMOVE = 1 ] || [ $ADD = 1 ] || [ $CREATE = 1 ] || [ $UPDATE = 0 ] &&
-			{
-				show_help
-				exit 1
-			}
-			INSTALL=1
-			;;
-		h|\?)
-			show_help
-			exit 0
-			;;
-		:)
-			echo "Option -$OPTARG requires an argument." >&2
-			exit 1
-			;;
-	esac
+    case $opt in
+        e)
+            [ $CREATE = 1 ] &&
+            {
+                show_help
+
+                exit 1
+            }
+            UPDATE=1
+            ENVIRONMENT=$OPTARG
+            ;;
+        c)
+            [ $UPDATE = 1 ] || [ $ADD = 1 ] || [ $REMOVE = 1 ] || [ $INSTALL = 1 ] &&
+            {
+                show_help
+
+                exit 1
+            }
+            CREATE=1
+            ENVIRONMENT=$OPTARG
+            ;;
+        a)
+            [ $REMOVE = 1 ] || [ $INSTALL = 1 ] || [ $CREATE = 1 ] || [ $UPDATE = 0 ] &&
+            {
+                show_help
+
+                exit 1
+            }
+            ADD=1
+            ARG=$OPTARG
+            ;;
+        r)
+            [ $ADD = 1 ] || [ $INSTALL = 1 ] || [ $CREATE = 1 ] || [ $UPDATE = 0 ] &&
+            {
+                show_help
+
+                exit 1
+            }
+            REMOVE=1
+            ARG=$OPTARG
+            ;;
+        i)
+            [ $REMOVE = 1 ] || [ $ADD = 1 ] || [ $CREATE = 1 ] || [ $UPDATE = 0 ] &&
+            {
+                show_help
+
+                exit 1
+            }
+            INSTALL=1
+            ;;
+        h|\?)
+            show_help
+
+            exit 0
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+
+            exit 1
+            ;;
+    esac
 done
 
 if [ -z $ENVIRONMENT ]; then
-	show_help
-	exit 0
+    show_help
+
+    exit 0
 fi
 
 if [ $CREATE = 1 ]; then
-	create $ENVIRONMENT
-	exit 0
+    create_environment $ENVIRONMENT
+
+    exit 0
 fi
 
 cd $ENVIRONMENT
 
 if [ $ADD = 1 ]; then
-	add $ARG
+    add "$ARG"
 elif [ $REMOVE = 1 ]; then
-	remove $ARG
+    remove "$ARG"
 elif [ $INSTALL = 1 ]; then
-	install_environment
+    install_environment
 else
-	show_help
+    show_help
 fi
