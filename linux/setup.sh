77bce415bb9b19c61b4b5e0b176fe7d116b64261
fabienduhamel <fabienduhamel@users.noreply.github.com>
2015-04-21 20:16:15 +0200

    Added htop instead of top

--- linux/setup.sh
+++ linux/setup.sh
@@ -3,7 +3,7 @@
 # this scripts runs the very essential commands for a new linux installation
 
 # main packages
-sudo apt-get install terminator zsh vim dconf-tools
+sudo apt-get install terminator zsh vim dconf-tools htop
 
 # oh my zsh
 wget --no-check-certificate http://install.ohmyz.sh -O - | sh
