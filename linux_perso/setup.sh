3fe6b95a99ee1227d784a0e33338dfc8cac2a999
fabienduhamel <fabienduhamel@users.noreply.github.com>
2016-06-19 21:50:09 +0200

    Add wakeup support from wireless logitech keyboard

--- linux_perso/setup.sh
+++ linux_perso/setup.sh
@@ -3,3 +3,9 @@
 # Steam audio output switch issue with PulseAudio (remove DONT_MOVE flag for PulseAudio Steam sinks)
 # see http://steamcommunity.com/app/93200/discussions/0/864959809826195633/?l=french
 echo "[pulse]\nallow-moves=yes" > ~/.alsoftrc
+
+# Wakeup from wireless logitech keyboard
+git clone https://github.com/3v1n0/Solaar.git
+cd Solaar/rules.d
+./install.sh
+
